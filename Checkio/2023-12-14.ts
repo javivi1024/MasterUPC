e97c7634b57703681f82a5387f38126936eb5c3c
Javivi1024 <javi.ares@gmail.com>
2023-12-14 22:04:11 +0100

    2nd lesson

---
+++ Checkio/2023-12-14.ts
@@ -0,0 +1,21 @@
+// Lesson 2 - Is Even - Check if the given number is even or not
+function isEven(num: number): boolean {
+    return !(num % 2)
+       if (num % 2===0) {return true
+        
+    } else return false;
+}
+
+console.log(isEven(2));
+console.log(isEven(3));
+
+// Lesson 3 - Rectangle Perimeter - How to find the perimeter?
+function rectanglePerimeter(length: number, width: number): number {
+    // your code here
+    return 2 * length + 2 * width;
+}
+
+const rectanglePerimeter_arrow = (length: number, width: number): number => 2 * (length+width);
+
+console.log(rectanglePerimeter(3, 2));
+console.log(rectanglePerimeter_arrow(3, 2));
