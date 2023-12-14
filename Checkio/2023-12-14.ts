140d8816f2704abef302b3555120eabf129a79a1
javivi1024 <85297013+javivi1024@users.noreply.github.com>
2023-12-14 22:05:46 +0100

    Update 2023-12-14.ts

--- Checkio/2023-12-14.ts
+++ Checkio/2023-12-14.ts
@@ -10,11 +10,12 @@ console.log(isEven(2));
 console.log(isEven(3));
 
 // Lesson 3 - Rectangle Perimeter - How to find the perimeter?
+//Utilizando una función clasica
 function rectanglePerimeter(length: number, width: number): number {
     // your code here
     return 2 * length + 2 * width;
 }
-
+// Utilizando un arrow function
 const rectanglePerimeter_arrow = (length: number, width: number): number => 2 * (length+width);
 
 console.log(rectanglePerimeter(3, 2));
